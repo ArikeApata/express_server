72eb756ba1954a7a511c06aff887b9f2eb63ccbc
freemile2ru <sofiyullahj@gmail.com>
2023-04-16 13:36:57 +0100

    new changes

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log(req.user);
-});
-
-router.get("/new", (req, res) => {
-  res.render("users/new");
-});
-
-router.post("/", (req, res) => {
-  console.log("data", req.body);
-  const isValid = true;
-  if (isValid) {
-    //users.push({ name: req.body.name });
-    //res.redirect(`/users/${users.length - 1}`);
-  } else {
-    console.log("Error");
-    res.render("users/new", { name: req.body.name });
-  }
-
-  // console.log(req.body.users)
-});
-
-router
-  .route("/:id")
-  .get((req, res) => {
-    console.log(req.user);
-    res.send(`added user with ID ${req.params.id}`);
-  })
-  .put((req, res) => {
-    res.send(`new user has ID ${req.params.id}`);
-  });
-
 const users = [
   {
     name: "Olamilekan",
@@ -47,9 +15,17 @@ const users = [
     id: 2,
   },
 ];
-router.param("id", (req, res, next, id) => {
-  req.user = users[id];
-  next();
-});
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.send(users);
+  })
+  .post((req, res) => {
+    let data = req.body;
+    data.id = users.length + 1;
+    users.push(req.body);
+    res.json({ msg: "data added succesfully", data });
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
 
 app.use("/users", userRouter);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
